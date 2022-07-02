28b025abe36b29efc6b94592bdd7f902d0ce7d1d
Samuel Marroni <samuelmarroni@hotmail.com>
2022-07-02 18:06:15 -0300

    Adicionando header e footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,18 +2,23 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
     <title>{{ $title }}</title>
 </head>
 <body>
-    <header>
-        
+    <header class="app-header">
+        <img id="logo" src="/img/logo.svg" alt="Logo">
+        <div class="admin">
+            <span>Olá, Admin!</span>
+            <img id="avatar" src="/img/admin.svg" alt="Avatar">
+        </div>
     </header>
     <h1>{{ $title }}</h1>
 
     {{ $slot }}
 
-    <footer>
-
+    <footer class="app-footer">
+        <span>Samuel {{ date('Y') }}</span>
     </footer>
 </body>
 </html>
--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Department">
-    <p>eraiueabvbruasebvryubvue</p>
+    
 </x-layout>
